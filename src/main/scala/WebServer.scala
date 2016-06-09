5e09688fa98a0b23e6ab3d41f06ea8fc9260cfab
Dima Stazenko <dima.stazenko@clicktale.com>
2016-06-09 17:34:50 +0300

    Initial commit

---
+++ src/main/scala/WebServer.scala
@@ -0,0 +1,40 @@
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.HttpMethods._
+import akka.http.scaladsl.model._
+import akka.stream.ActorMaterializer
+
+import scala.io.StdIn
+
+object WebServer {
+  def main(args: Array[String]) {
+    implicit val system = ActorSystem()
+    implicit val materializer = ActorMaterializer()
+
+    // needed for the future map / flatmap in the end
+    implicit val executionContext = system.dispatcher
+
+    val requestHandler: HttpRequest => HttpResponse = {
+
+      case HttpRequest(GET, Uri.Path("/"), _, _, _) =>
+        HttpResponse(entity = HttpEntity(
+          ContentTypes.`text/html(UTF-8)`,"<html><body>Hello world!</body></html>"))
+
+      case HttpRequest(GET, Uri.Path("/ping"), _, _, _) =>
+        HttpResponse(entity = "PONG!")
+
+      case HttpRequest(GET, Uri.Path("/crash"), _, _, _) =>
+        sys.error("BOOM!")
+
+      case _: HttpRequest =>
+        HttpResponse(404, entity = "Unknown resource!")
+    }
+
+    val bindingFuture = Http().bindAndHandleSync(requestHandler, "localhost", 8080)
+    println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+
+    StdIn.readLine() // let it run until user presses return
+    bindingFuture.flatMap(_.unbind()).onComplete(_ => system.terminate())
+
+  }
+}
